296bc66a0654beafd8fd1b1d76061dfd0aa16585
Diane Candler <dianedcandler@gmail.com>
2020-05-23 17:02:42 -0500

    delete extra folder

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
 	console.log('ready!'); //-- inside document function to test JQuery ready
+	// Weather API
+	var appID = "2c820066ae7ac5a97f92d60b8422d7ff"
+// below is copied from homework
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	  }).then(function(response) {
+		$("#movies-view").text(JSON.stringify(response));
+	  });
 });
